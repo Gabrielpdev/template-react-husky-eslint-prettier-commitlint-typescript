9a915c78d053a7c407ea27e1039a70fc1e575bb2
Gabriel Oliveira <gabriel9938@gmail.com>
2022-12-01 09:33:31 -0300

    chore: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,11 +13,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          asdasdas sad asd asd adasdsdas
         </a>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
